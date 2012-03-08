5ce58e41a48096535c5670e32b565ae67b4a0771
Ed Willson <ed.willson24@gmail.com>
2012-03-08 22:33:01 +0000

    Added is_arithmetic<T>() to rand_impl() overloads.

--- Cea/src/rvar/uniform.hpp
+++ Cea/src/rvar/uniform.hpp
@@ -1,6 +1,6 @@
 // Uniform distribution.
-#ifndef CEA_UNIFORM_H
-#define CEA_UNIFORM_H
+#ifndef CEA_UNIFORM_HPP
+#define CEA_UNIFORM_HPP
 
 template <typename T>
 class RVarUniform : public RVar<T>
@@ -30,21 +30,22 @@ class RVarUniform : public RVar<T>
         // Use type traits to determine which (private) implementation to use.
         T rand()
         {
-            return rand_impl(std::is_floating_point<T>());
+            return rand_impl(std::is_arithmetic<T>(), std::is_floating_point<T>());
         }
 
     private:
         // Typed rand() overloads.
-        inline T rand_impl(std::true_type)
+        inline T rand_impl(std::true_type, std::true_type)
         {
             T temp = static_cast<T>(source->rand()) / static_cast<T>(std::numeric_limits<uint64_t>::max());
             return lower + (temp * (upper - lower));
         }
 
-        inline T rand_impl(std::false_type)
+        inline T rand_impl(std::true_type, std::false_type)
         {
             return static_cast<T>(lower + (source->rand() % (upper - lower)));
         }
+
         // Bounds
         T lower;
         T upper;
@@ -53,4 +54,4 @@ class RVarUniform : public RVar<T>
         Prng * source;
 };
 
-#endif // CEA_UNIFORM_H
+#endif // CEA_UNIFORM_HPP
